aa502607be39478206cf498801b7da09ada061d3
Paweł Kołaczyński <kolaczyn@protonmail.com>
2021-01-04 15:25:40 +0100

    Finished home page (3:10)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,33 @@
-import React from 'react'
+import React from "react";
 import FaqContainer from "../containers/Faq";
+import { Feature, OptForm } from "../components";
 import FooterContainer from "../containers/Footer";
+import HeaderContainer from "../containers/Header";
 import JumbotronContainer from "../containers/Jumbotron";
 
 export default function Home() {
   return (
-     <> 
-        <JumbotronContainer />
-        <FaqContainer />
-        <FooterContainer />
-      </>
-  )
+    <>
+      <HeaderContainer>
+        <Feature>
+          <Feature.Title>Unlimited fims, TV programmes and more.</Feature.Title>
+          <Feature.SubTitle>
+            Watch anywhere. Cancel at any time.
+          </Feature.SubTitle>
+        <OptForm>
+          <OptForm.Input placeholder="Email address" />
+          <OptForm.Button>Try it now</OptForm.Button>
+          <OptForm.Break />
+          <OptForm.Text>
+            Ready to watch? Enter your email to create or restart your
+            membership
+          </OptForm.Text>
+        </OptForm>
+        </Feature>
+      </HeaderContainer>
+      <JumbotronContainer />
+      <FaqContainer />
+      <FooterContainer />
+    </>
+  );
 }
